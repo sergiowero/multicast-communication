b941f760936a5cd5a94254f26d36fc98f2980884
Sergio Sanchez <sergioj.sanchezr@gmail.com>
2017-07-21 17:52:01 -0500

    Added code
    Added presentation code for TCP and UDP

--- src/tcpServer/TCPServer.java
+++ src/tcpServer/TCPServer.java
@@ -1,6 +1,16 @@
-
+import java.net.*;
+import java.io.*;
 public class TCPServer {
-    public static void main(String[] args) {
-        System.out.println("Soy el TCP Server");
+    public static void main (String args[]) {
+        try{
+            int serverPort = 7896;
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+            while(true) {
+                Socket clientSocket = listenSocket.accept();
+                new Connection(clientSocket);
+            }
+        } catch (IOException e) {
+            System.out.println("Listen :" + e.getMessage());
+        }
     }
 }
